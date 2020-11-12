cffd8f1f66a7ca482ec1b37186a0731ef6b32707
kamalakars04 <kamalakars04@gmail.com>
2020-11-12 23:42:15 +0530

    [Kamalakar].[Refactor].Added UC2_AddEmployees

--- EmployeePayrollRestSharp/UnitTest1.cs
+++ EmployeePayrollRestSharp/UnitTest1.cs
@@ -3,6 +3,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using RestSharp;
 using Newtonsoft.Json;
 using System;
+using Newtonsoft.Json.Linq;
 
 namespace EmployeePayrollRestSharp
 {
@@ -43,6 +44,44 @@ namespace EmployeePayrollRestSharp
             // executing the request using client and saving the result in IrestResponse.
             IRestResponse response = client.Execute(request);
             return response;
+        }
+
+        /// <summary>
+        /// TC 2 Givens the employee on post should return added employee.
+        /// </summary>
+        [TestMethod]
+        public void givenEmployee_OnPost_ShouldReturnAddedEmployee()
+        {
+            //adding multiple employees to list
+            List<Employee> MultipleEmployeeList = new List<Employee>();
+            MultipleEmployeeList.Add(new Employee { name = "Raja", salary = 15421 });
+            MultipleEmployeeList.Add(new Employee { name = "Rani", salary = 52300 });
+            MultipleEmployeeList.ForEach(employeeData =>
+            {
+                // Arrange
+                RestRequest request = new RestRequest("/Employee", Method.POST);
+
+                // Create jObject for adding data for name and salary, id auto increments
+                JObject jObject = new JObject();
+                jObject.Add("name", employeeData.name);
+                jObject.Add("salary", employeeData.salary);
+
+                // parameters are passed as body hence "request body" call is made, in parameter type
+                request.AddParameter("application/json", jObject, ParameterType.RequestBody);
+
+                //Act
+                IRestResponse response = client.Execute(request);
+
+                // Assert
+                // code will be 201 for posting data
+                Assert.AreEqual(response.StatusCode, System.Net.HttpStatusCode.Created);
+
+                // Derserializing object for assert and checking test case
+                Employee dataResponse = JsonConvert.DeserializeObject<Employee>(response.Content);
+                Assert.AreEqual(employeeData.name, dataResponse.name);
+                Console.WriteLine(response.Content);
+            });
+
         }
     }
 }
